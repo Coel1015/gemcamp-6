b71b66a534ab466d729b2bae073fc97e018d2722
ruby_08 <ruby_08@swzs-iMac.local>
2024-04-01 17:28:41 +0800

    updated active record

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'posts/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
@@ -8,4 +9,8 @@ Rails.application.routes.draw do
   get 'contact', to: 'welcome#contact'
   get 'about', to: 'welcome#about'
   get 'portfolio', to: 'welcome#portfolio'
+  get 'index', to: 'posts#index'
+  # root 'welcome#index'
+  #
+  resources :posts
 end
